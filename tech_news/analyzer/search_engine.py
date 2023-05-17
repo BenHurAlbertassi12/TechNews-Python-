9793f860489102d9a5427858e5f7add73c892ee4
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-05-16 23:38:39 -0300

    Req 8 / 9

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,5 +1,6 @@
 # Requisito 7
 from tech_news.database import search_news
+from datetime import datetime
 
 
 def search_by_title(title):
@@ -11,8 +12,20 @@ def search_by_title(title):
 # Requisito 8
 def search_by_date(date):
     """Seu código deve vir aqui"""
+    try:
+        data = datetime.strptime(date, '%d/%m/%Y')
+        org_data = data.strftime('%Y-%m-%d')
+        return [(new['title'], new['url'])
+                for new in search_news({'timestamp': org_data})]
+    except ValueError:
+        raise ValueError('Data inválida')
 
 
 # Requisito 9
 def search_by_category(category):
     """Seu código deve vir aqui"""
+    list_arr = []
+    noticias = search_news({"category": {"$regex": category, "$options": "i"}})
+    if noticias:
+        list_arr = [(new["title"], new["url"]) for new in noticias]
+    return list_arr
